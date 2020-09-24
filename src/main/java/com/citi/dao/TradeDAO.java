7700ed7c451100968b52e71c3edfa0a9610ea52b
Uday Vig <uday.vig@gmail.com>
2020-09-25 00:39:45 +0530

    Trade Bean and DAO

--- src/main/java/com/citi/dao/TradeDAO.java
+++ src/main/java/com/citi/dao/TradeDAO.java
@@ -1,5 +1,18 @@
 package com.citi.dao;
 
-public interface TradeDAO {
+import java.util.List;
 
+import com.citi.bean.Trade;
+
+public interface TradeDAO {
+	
+	Trade getTradeByTradeID (int tradeID);
+	
+	List<Trade> getTradeByStockID (int stockID);
+	
+	List<Trade> getTradeBySellerID (int sellerID);
+	
+	List<Trade> getTradeByBuyerID (int buyerID);
+	
+	List<Trade> getAllTrades ();
 }
--- src/main/java/com/citi/dao/TradeDAOImpl.java
+++ src/main/java/com/citi/dao/TradeDAOImpl.java
@@ -1,5 +1,39 @@
 package com.citi.dao;
 
+import java.util.List;
+
+import com.citi.bean.Trade;
+
 public class TradeDAOImpl implements TradeDAO {
+
+	@Override
+	public Trade getTradeByTradeID(int tradeID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Trade> getTradeByStockID(int stockID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Trade> getTradeBySellerID(int sellerID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Trade> getTradeByBuyerID(int buyerID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Trade> getAllTrades() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
